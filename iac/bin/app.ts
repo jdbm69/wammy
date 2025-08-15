7cc8b30cf7e98ae9c95ea2cc7d9860105c72c0dc
jdbm69 <jhonnydevb@gmail.com>
2025-08-15 13:36:07 -0300

    changes

--- iac/bin/app.ts
+++ iac/bin/app.ts
@@ -2,9 +2,13 @@
 
 // Entrypoint de la app CDK, carga variables de entorno, crea la App y despliega la instancia de WamyStack
 
-import 'dotenv/config';   
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+
 import * as cdk from 'aws-cdk-lib';   
 import { WamyStack } from '../lib/stack'; 
+
+dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const app = new cdk.App();
 new WamyStack(app, 'WamyWindAggregatorStack', {
